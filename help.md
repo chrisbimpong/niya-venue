15ac61a3f82bcf9dfc9cf07cb8dc4a9d8bcb2b96
josoftme <joshuaiheji@gmail.com>
2017-02-11 11:53:58 +0000

    Firebase initialization and help file
    Initialized firebase in the project with the url
    niya-venue.firebaseio.com. Added the NiyaVenue function to script.js for
    binding firebase methods.
    
    Created a help.md file to document the commands used.

---
+++ help.md
@@ -0,0 +1,7 @@
+## Guide 
+
+###Initialize Firebase (Use the firebase console to first create the app)
+`firebase init; firebase use --add; firebase serve`
+
+
+
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,27 @@
 /*
  * Title:   NiYa | Book Your Perfect Venue - Custom Javascript file
- * Author:  http://themeforest.net/user/soaptheme
+ * Author:  http://github.com/joshuaai
  */
 
 tjq(document).ready(function() {
     // UI Form Element
 });
+
+//Initializes Niya Venue
+function NiyaVenue() {
+    this.checkSetup();
+
+    //Shortcuts to DOM elements
+    this.messageList = document.getElementById('messages');
+    this.messageForm = document.getElementById('message-form');
+    this.messageInput = document.getElementById('message');
+    this.submitButton = document.getElementById('submit');
+    this.submitImageButton = document.getElementById('submitImage');
+    this.imageForm = document.getElementById('image-form');
+    this.mediaCapture = document.getElementById('mediaCapture');
+    this.userPic = document.getElementById('user-pic');
+    this.userName = document.getElementById('user-name');
+    this.signInButton = document.getElementById('sign-in');
+    this.signOutButton = document.getElementById('sign-out');
+
+}
