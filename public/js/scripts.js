304739c842a8bc588c611bb0bfed75e6402cdd5d
josoftme <joshuaiheji@gmail.com>
2017-02-11 11:04:34 +0000

    First commit with complete app UI

---
+++ public/js/scripts.js
@@ -0,0 +1,8 @@
+/*
+ * Title:   NiYa | Book Your Perfect Venue - Custom Javascript file
+ * Author:  http://themeforest.net/user/soaptheme
+ */
+
+tjq(document).ready(function() {
+    // UI Form Element
+});
