c550e7fb2dd0e1508f5c9c22c02f11f8990c2d41
josoftme <joshuaiheji@gmail.com>
2017-02-11 11:58:35 +0000

    Firebase initialization and help file

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -24,4 +24,44 @@ function NiyaVenue() {
     this.signInButton = document.getElementById('sign-in');
     this.signOutButton = document.getElementById('sign-out');
 
+    // Saves message on form submit.
+    this.messageForm.addEventListener('submit', this.saveMessage.bind(this));
+    this.signOutButton.addEventListener('click', this.signOut.bind(this));
+    this.signInButton.addEventListener('click', this.signIn.bind(this));
+
+    // Toggle for the button.
+    var buttonTogglingHandler = this.toggleButton.bind(this);
+    this.messageInput.addEventListener('keyup', buttonTogglingHandler);
+    this.messageInput.addEventListener('change', buttonTogglingHandler);
+
+    // Events for image upload.
+    this.submitImageButton.addEventListener('click', function() {
+        this.mediaCapture.click();
+    }.bind(this));
+    this.mediaCapture.addEventListener('change', this.saveImageMessage.bind(this));
+
+    this.initFirebase();
 }
+
+// Sets up shortcuts to Firebase features and initiate firebase auth.
+NiyaVenue.prototype.initFirebase = function() {
+    // Shortcuts to Firebase SDK features.
+    this.auth = firebase.auth();
+    this.database = firebase.database();
+    this.storage = firebase.storage();
+    // Initiates Firebase auth and listen to auth state changes.
+    this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+}
+
+// Signs-in with Google.
+NiyaVenue.prototype.signIn = function() {
+  // Sign in Firebase using popup auth and Google as the identity provider.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  this.auth.signInWithPopup(provider);
+};
+
+// Signs-out of Google.
+NiyaVenue.prototype.signOut = function() {
+  // Sign out of Firebase.
+  this.auth.signOut();
+};
